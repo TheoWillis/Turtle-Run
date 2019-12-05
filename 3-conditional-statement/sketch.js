4b0e7b3e88b23c980db9558212610ed77c42bd36
TheoWillis <theowillis484@gmail.com>
2019-12-05 15:05:53 -0800

    made everything right again

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -24,15 +24,15 @@ function preload() {
 
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(800, 400);
 
   //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
+  me = new Avatar(width/2, 300, 3, true);
 }
 
 function draw(){
+  background(66, 135, 245);
 
-	background(66, 135, 245);
   textSize(30);
     fill(22, 161, 10);
   text ("Score: " + score,50,50)
@@ -63,11 +63,11 @@ function draw(){
   if (frameCount % 80 == 0) {
       let  b = new Ball(width, random(0,height), -3, false);
 
-	
+
 
   animation(ghost, 300, 300);
 
-  
+
 
       balls.push(b);
       console.log(balls); //print the balls array to the console
@@ -97,10 +97,11 @@ function draw(){
 //avatar class
 class Avatar {
 
-	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+	constructor(x,y, speed,alive){ //every avatar needs an x value, a y value, and a speed
 		    this.x = x;
     		this.y = y;
         this.speed = speed;
+        this.alive = alive;
 	}
 
 	drawMe(){  // draw the running person
@@ -119,15 +120,24 @@ class Avatar {
 
 	}
 
-	moveMe(){
-    if (keyIsPressed===true) { //if you hold the up arrow, move up by speed
-      this.x = this.x+5+ this.speed;
-      this.y = this.y+10+this.speed;
+  moveMe(){
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -=this.speed*2;
     }
+   if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
+      this.x += this.speed*2;
+    
+    }
+    if (keyIsDown(UP_ARROW)) {
+        this.y -= this.speed;
+    }
+    if(keyIsDown(DOWN_ARROW)) {
+        this.y += this.speed;
 
   //  if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
       //  this.y += this.speed;
     }
+  }
 	}
 
   // die(){
@@ -140,24 +150,25 @@ class Avatar {
 class Ball {
 
 	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
+	constructor(x,y, speed,hit){
 		this.x = x;
     this.y = y;
     this.speed = speed;
+    this.hit = hit;
 	}
 
 	// draw a ball on the screen at x,y
 	drawBall(){
     	stroke(0);
       strokeWeight(1);
-    	fill("red");
-		  ellipse(this.x,this.y,10,10);
+      fill("brown");
+		 rect(this.x,this.y,100,35,10);
 	}
 
 	//update the location of the ball, so it moves across the screen
 	moveBall(){
 		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
+		this.y = this.y;
 	}
 
 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
