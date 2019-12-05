a5b2c6238d7ee734593a91ed6fde265150c0d990
melindaliu <meilnda.r.liu@gmail.com>
2019-12-05 14:32:10 -0800

    files

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -1,37 +1,37 @@
 //create an empty array called balls
 let balls = [];
-let score = 0;
 
 //create a variable to hold your avatar
 let me;
 
+var ghost;
+//
+// //it's advisable (but not necessary) to load the images in the preload function
+// //of your sketch otherwise they may appear with a little delay
+function preload() {
+//
+//   //create an animation from a sequence of numbered images
+//   //pass the first and the last file name and it will try to find the ones in between
+  ghost = loadAnimation('sprites/Turtle1.png', 'sprites/Turtle2.png','sprites/Turtle3.png','sprites/Turtle4.png');
+}
+
+
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(500, 400);
 
   //make one avatar called me
-  me = new Avatar(width/2, 300, 3,true);
+  me = new Avatar(width/2, 300, 3);
 }
 
 function draw(){
-	background(66, 135, 245);
-  textSize(30);
-  text (score,50,50)
-
+	background(255,20,255);
   me.drawMe();
   me.moveMe();
 
-  if(me.alive==true){
-    if (frameCount%60==0){
-      score=score+1
-      }
-    }
+  animation(ghost, 300, 300);
 
-  else{
-      score=0
-    }
-
-  if (frameCount % 80 == 0) {
-      let  b = new Ball(width, random(0,height), -3, false);
+  if (frameCount % 40 == 0) {
+      let  b = new Ball(width, random(0,height), -3);
       balls.push(b);
       console.log(balls); //print the balls array to the console
     }
@@ -40,85 +40,79 @@ function draw(){
 	for (let i = 0; i < balls.length; i++) {
 	 	      balls[i].drawBall();
        	  balls[i].moveBall();
-          balls[i].bounceBall();
-     }
+        	balls[i].bounceBall();
+	  }
+
 }
 
 //avatar class
 class Avatar {
 
-	constructor(x,y, speed,alive){ //every avatar needs an x value, a y value, and a speed
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
 		    this.x = x;
     		this.y = y;
         this.speed = speed;
-        this.alive = alive;
 	}
 
 	drawMe(){  // draw the running person
-    if(this.alive==true){
-      stroke("green");
-      strokeWeight(3);
-      fill(22, 161, 10);
-      ellipse(this.x,this.y,40,40);
-        }
-    else{
-      textSize(60)
-      fill("red")
-      text('YOU DIED',175,175)
-        }
+    		stroke("green");
+        strokeWeight(3);
+    		fill("blue");
+		    ellipse(this.x,this.y,20,20);
+        line(this.x,this.y, this.x, this.y+40);
+        line(this.x, this.y+40, this.x-20, this.y+60);
+        line(this.x, this.y+40, this.x+10, this.y+50);
+        line(this.x+10, this.y+50, this.x+5, this.y+60);
+        line(this.x, this.y+15, this.x-10, this.y+25);
+        line(this.x-10, this.y+25, this.x+10, this.y+35);
 	}
 
 	moveMe(){
-    if (keyIsDown(LEFT_ARROW)) {
-      this.x -=this.speed*2;
+    if (keyIsPressed===true) { //if you hold the up arrow, move up by speed
+      this.x = this.x+5+ this.speed;
+      this.y = this.y+10+this.speed;
     }
-   if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
-      this.x += this.speed*2;
+
+  //  if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
+      //  this.y += this.speed;
     }
-    if (keyIsDown(UP_ARROW)) {
-        this.y -= this.speed;
-    }
-    if(keyIsDown(DOWN_ARROW)) {
-        this.y += this.speed;
-    }
-  }
-}
+	}
+
   // die(){
   //
   // }
+
+
 
 //ball class from which to create new balls with similar properties.
 class Ball {
 
 	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed, hit){
+	constructor(x,y, speed){
 		this.x = x;
     this.y = y;
     this.speed = speed;
-    this.hit= hit;
 	}
 
 	// draw a ball on the screen at x,y
 	drawBall(){
     	stroke(0);
       strokeWeight(1);
-    	fill("brown");
-		 rect(this.x,this.y,100,35,10);
+    	fill("red");
+		  ellipse(this.x,this.y,10,10);
 	}
 
-	//update the location of the ball, so it moves cross the screen
+	//update the location of the ball, so it moves across the screen
 	moveBall(){
-		this.x = this.x+this.speed;
-  //this.-# changes how fast the logs move from right to left
-		this.y = this.y;
-  }
+		this.x = this.x+ this.speed;
+		this.y = this.y+.5;
+	}
 
+	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
   bounceBall(){
-    //  if (me.x+20>=this.x && me.y-20>=this.y &&me.y+20<=this.y){
-      if (me.x>=this.x && me.x<=this.x+10 && me.y-20<=this.y+35&&this.hit==false&&me.y+20>=this.y){
-        this.speed=this.speed;
-        this.hit=true;
-        me.alive=false;
-      }
-  }
+    if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
+      	this.speed = -this.speed;
+    		}
+  	}
+
 }
